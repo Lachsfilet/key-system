ab20e534ab653272b3ff4ffd32a98e6d3c33b6e1
Lachsfilet <99832883+Lachsfilet@users.noreply.github.com>
2024-01-21 13:27:24 +0000

    started from scratch product key generator implemented no use yet

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const { REST, Routes } = require('discord.js');
 const axios = require('axios');
 const config = require('./config.json');
-const api = require('./apikey.json');
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const commands = [
@@ -15,7 +14,20 @@ const rest = new REST({ version: '10' }).setToken(config.token);
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const apiUrl = 'http://localhost:3000/create-key';
+function generateProductKey() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const keyLength = 16;
+  let productKey = "";
+
+  for (let i = 0; i < keyLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    productKey += characters.charAt(randomIndex);
+  }
+
+  const formattedKey = productKey.replace(/(.{4})/g, "$1-").slice(0, -1);
+  return formattedKey;
+  // todo add save system for productkeys with buyer check for product use
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -27,22 +39,10 @@ client.on('interactionCreate', async (interaction) => {
   const { commandName } = interaction;
 
   if (commandName === 'create-key') {
-    try {
-      const response = await axios.post(apiUrl, { apiKey: api.apiKey });
-      console.log('API Response:', response.data);
-
       await interaction.reply({
-        content: `API Key Valid! Here's your product key: ${response.data.productKey}`,
+        content: `API Key Valid! Here's your product key: ${generateProductKey()}`,
       });
-    } catch (error) {
-      console.error('Error:', error.message);
-      if (error.response) {
-        console.error('Response Status:', error.response.status);
-        console.error('Response Data:', error.response.data);
-      }
-      await interaction.reply('An error occurred while processing the command.');
     }
-  }
 });
 
 
